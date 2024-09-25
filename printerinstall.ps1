4194a73a1b35d45bcf75c9233ae43e538799d623
Chandler <chandlercalvert76@gmail.com>
2024-09-25 13:55:49 -0500

    Update printerinstall.ps1
    Added $driverLocation Variable
    
    Added /subdirs to make adding the driver to the driver repository look into subdirectories of the $extractFolderName.

--- printerinstall.ps1
+++ printerinstall.ps1
@@ -1,13 +1,15 @@
 # Variables for customization
-$downloadLink = "https://download.support.xerox.com/pub/drivers/ALC80XX/drivers/win10x64/ar/AltaLinkC80xx_7.76.0.0_PCL6_x64.zip"
+$downloadLink = "https://download.support.xerox.com/pub/drivers/GLOBALPRINTDRIVER/drivers/win10x64/ar/UNIV_5.1009.1.0_PCL6_x64.zip"
 $downloadPath = "C:\2Geeks"
-$zipFileName = "AltaLinkC80xx_7.76.0.0_PCL6_x64.zip"
-$extractFolderName = "AltaLinkC80xx_7.76.0.0_PCL6_x64"
-$infFileName = "XeroxAltaLinkC80xx_PCL6.inf"
-$driverName = "Xerox AltaLink C8035 V4 PCL6"
+$zipFileName = "UNIV_5.1009.1.0_PCL6_x64.zip"
+$extractFolderName = "UNIV_5.1009.1.0_PCL6_x64"
+$infFileName = "x3UNIVX.inf"
+$driverName = "Xerox Global Print Driver PCL6"
+# Driver location is the name of the folder in the File Repository where the driver INF file is located.
+$driverLocation = "x3univx.inf_amd64_da1d17edb334b897"
 $printerName = "Xerox Printer"
 $portName = "Copier_IP"
-$printerIPAddress = "10.0.1.9"
+$printerIPAddress = "192.168.100.50"
 
 # Create a folder for the files to be downloaded
 New-Item -Path $downloadPath -ItemType Directory
@@ -19,10 +21,10 @@ curl $downloadLink -o "$downloadPath\$zipFileName"
 Expand-Archive -Path "$downloadPath\$zipFileName" -DestinationPath "$downloadPath\$extractFolderName"
 
 # Add the printer driver .inf file to the driver store
-pnputil /add-driver "$downloadPath\$extractFolderName\$infFileName"
+pnputil /add-driver "$downloadPath\$extractFolderName\$infFileName" /subdirs
 
 # Add the printer driver to the computer
-Add-PrinterDriver -Name $driverName -InfPath "C:\Windows\System32\DriverStore\FileRepository\$infFileName"
+Add-PrinterDriver -Name $driverName -InfPath "C:\Windows\System32\DriverStore\FileRepository\$driverLocation\$infFileName"
 
 # Add a port to the computer
 Add-PrinterPort -Name $portName -PrinterHostAddress $printerIPAddress
@@ -30,6 +32,8 @@ Add-PrinterPort -Name $portName -PrinterHostAddress $printerIPAddress
 # Add the printer
 Add-Printer -DriverName $driverName -Name $printerName -PortName $portName
 
+# Remove the Zip file downloaded
 Remove-Item "$downloadPath\$zipFileName" -Recurse
 
+# Remove the extracted folder downloaded
 Remove-Item "$downloadPath\$extractFolderName" -Recurse
