e6e25e770fde9067acf3fbd4a19c63df171c3b68
Chandler <chandlercalvert76@gmail.com>
2025-01-28 21:29:10 -0600

    IPSW and iTunes Backup Scripts

---
+++ IPSW_Delete.ps1
@@ -0,0 +1,22 @@
+﻿#IPSW Cleanup Variables
+$IPSWDirectory = "$env:USERPROFILE\AppData\Roaming\Apple Computer\iTunes"
+$iPadFolder = "iPad Software Updates"
+$iPhoneFolder = "iPhone Software Updates"
+
+#Backup Cleanup Variables
+$BackupDirectory = "$env:USERPROFILE\AppData\Roaming\Apple Computer\MobileSync"
+$BackupDirectory1 = "$env:USERPROFILE\Apple\MobileSync"
+$BackupFolder = "Backup"
+
+#IPSW Delete IPSW files folders and recreate them.
+cd $IPSWDirectory
+Remove-Item -Path $iPhoneFolder, $iPadFolder -Recurse -Force
+New-Item -Path $iPhoneFolder, $iPadFolder
+
+#Backup folder delete and recreate
+cd $BackupDirectory
+Remove-Item -Path $BackupFolder -Recurse -Force
+New-Item -Path $BackupFolder
+cd $BackupDirectory1
+Remove-Item -Path $BackupFolder -Recurse -Force
+New-Item -Path $BackupFolder
---
+++ iTunes_Backup_Delete.ps1
@@ -0,0 +1,12 @@
+﻿#Backup Cleanup Variables
+$BackupDirectory = "$env:USERPROFILE\AppData\Roaming\Apple Computer\MobileSync"
+$BackupDirectory1 = "$env:USERPROFILE\Apple\MobileSync"
+$BackupFolder = "Backup"
+
+#Backup folder delete and recreate
+cd $BackupDirectory
+Remove-Item -Path $BackupFolder -Recurse -Force
+New-Item -Path $BackupFolder
+cd $BackupDirectory1
+Remove-Item -Path $BackupFolder -Recurse -Force
+New-Item -Path $BackupFolder
